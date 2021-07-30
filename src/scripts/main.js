47e6492b03322dd59bbca362cb5bc07702967fc6
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2021-07-30 09:35:42 -0700

    removing dj mixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,7 +2,7 @@ import requestEventsXHR from './components/requestEvents';
 import requestLocationsXHR from './components/requestLocations';
 import requestImagesXHR from './components/requestImages';
 import navigationDropdowns from './components/navigationDropdowns';
-import requestPostsAndAttachtoPage from './components/requestsPosts';
+// import requestPostsAndAttachtoPage from './components/requestsPosts';
 
 // Initialize Application
 
@@ -10,4 +10,4 @@ let locations = requestLocationsXHR();
 requestEventsXHR(81, locations);
 requestImagesXHR();
 navigationDropdowns();
-requestPostsAndAttachtoPage('music', 8);
+// requestPostsAndAttachtoPage('music', 8);
