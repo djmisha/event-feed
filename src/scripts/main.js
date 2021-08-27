e7d3c5828ff7de519789ebacc93e84155aafd6eb
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2021-08-27 08:39:27 -0700

    User Location by IP

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,16 @@
-import requestEvents from './components/requestEvents';
-import requestEventsByIP from './components/requestEventsByIP';
-import requestLocationsXHR from './components/requestLocations';
 import navigationDropdowns from './components/navigationDropdowns';
 import navigationMobile from './components/navigationMobile';
+import getIPAddress from './components/getIPAddress'
 // import requestPostsAndAttachtoPage from './components/requestsPosts';
 
 // Initialize Application
 
-let locations = requestLocationsXHR();
-requestEvents(81, locations);
-// requestEventsByIP(locations);
+const init = async () => {
+  await getIPAddress();
+}
+
+init();
+
 navigationDropdowns();
 navigationMobile();
 
