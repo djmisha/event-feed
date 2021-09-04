10eaea447acaf533b3de4030a2a2f786db79a4c9
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2021-09-03 18:30:56 -0700

    fuck

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,31 +2,33 @@ import navigationDropdowns from './components/navigationDropdowns';
 import navigationMobile from './components/navigationMobile';
 import getIPAddress from './components/getIPAddress'
 import getLocationID from './components/getLocationID';
-import activateNav from './components/navigation'
+// import activateNav from './components/navigation'
 // import requestPostsAndAttachtoPage from './components/requestsPosts';
 
 // Initialize Application
 
-const isReturnUser = () => {
-  var city = localStorage.getItem("city", city);
-  var state = localStorage.getItem("state", state);
-  if (city && state) {
-    getLocationID(city, state);
-  }
-  else {
-    setUserLocation();
-  }
-}
+// const isReturnUser = () => {
+//   var city = localStorage.getItem("city", city);
+//   var state = localStorage.getItem("state", state);
+//   if (city && state) {
+//     getLocationID(city, state);
+//   }
+//   else {
+// }
+// }
+
+// isReturnUser();
 
 const setUserLocation = async () => {
   await getIPAddress();
 }
 
-isReturnUser();
+setUserLocation();
+
 
 navigationDropdowns();
 navigationMobile();
 
-activateNav();
+// activateNav();
 
 // requestPostsAndAttachtoPage('music', 8);
